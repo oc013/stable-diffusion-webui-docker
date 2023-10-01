a3e7c73445d30d12bb433412ea9f02af55b7f123
oc013 <101832295+oc013@users.noreply.github.com>
2023-10-01 00:33:46 -0400

    rootless

--- services/AUTOMATIC1111/entrypoint.sh
+++ services/AUTOMATIC1111/entrypoint.sh
@@ -43,12 +43,12 @@ MOUNTS["${ROOT}/repositories/CodeFormer/weights/facelib"]="/data/.cache"
 for to_path in "${!MOUNTS[@]}"; do
   set -Eeuo pipefail
   from_path="${MOUNTS[${to_path}]}"
-  rm -rf "${to_path}"
+  sudo rm -rf "${to_path}"
   if [ ! -f "$from_path" ]; then
     mkdir -vp "$from_path"
   fi
   mkdir -vp "$(dirname "${to_path}")"
-  ln -sT "${from_path}" "${to_path}"
+  sudo ln -sT "${from_path}" "${to_path}"
   echo Mounted $(basename "${from_path}")
 done
 
